2452e6b496fe783674c8c06009e6d5a11ebbe7b5
Samiul Hasan <samiulxhasan650@gmail.com>
2022-05-22 23:28:27 +0600

    Navbar error fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 import Footer from './shared/Footer';
 import Navbar from './shared/Navbar';
 
 function App() {
+
+
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='home' element={<Home></Home>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-      </Routes>
+      <div className="drawer drawer-end">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <div className="w-full navbar bg-primary px-48">
 
-      <Footer />
+            <div className="flex-1 px-2 mx-2">
+              <Link to='/'><h2 className='text-3xl font-bold text-white'>Exo Parts</h2></Link>
+            </div>
+            <div className="flex-none lg:hidden">
+              <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+              </label>
+            </div>
+
+            <div className="flex-none hidden lg:block">
+              <ul className="menu menu-horizontal">
+                <Navbar />
+              </ul>
+            </div>
+          </div>
+          {/* <!-- Page content here --> */}
+
+          <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='home' element={<Home></Home>}></Route>
+            <Route path='*' element={<NotFound></NotFound>}></Route>
+          </Routes>
+
+          <Footer />
+
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+          <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
+            <Navbar />
+
+          </ul>
+
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
