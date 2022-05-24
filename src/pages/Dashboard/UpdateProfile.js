4ff665058292fcb7c9c8c8b6b522ce4ae86abb6e
Samiul Hasan <samiulxhasan650@gmail.com>
2022-05-24 11:39:48 +0600

    user profile partial done

--- src/pages/Dashboard/UpdateProfile.js
+++ src/pages/Dashboard/UpdateProfile.js
@@ -1,9 +1,38 @@
 import React from 'react';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../fireabse.init';
 const UpdateProfile = () => {
+    const [user] = useAuthState(auth)
     return (
         <div>
-            <h2>Update Profile</h2>
+            <div class="card w-[60%] mx-auto bg-base-100 shadow-xl  border-2 my-10">
+                <h2 className='text-3xl font-bold text-secondary text-center py-8'>User Porfile</h2>
+                <div class="card-body">
+                    <div className='flex justify-between'>
+                        <div>
+                            <h3 className="text-2xl text-secondary">User Name</h3>
+                            <h3 className='text-secondary'>{user?.displayName}</h3>
+                            <h3 className="text-2xl text-secondary">User Email</h3>
+                            <h3 className='text-secondary'>{user?.email}</h3>
+                        </div>
+                        <div>
+                            <div className='rounded-[100px] overflow-hidden'>
+                                <img src={user?.photoURL} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <h3 className="text-2xl text-secondary">Phone</h3>
+                    <h3 className='text-secondary'>{user?.email}</h3>
+                    <h3 className="text-2xl text-secondary">Education</h3>
+                    <h3 className='text-secondary'>{user?.email}</h3>
+                    <h3 className="text-2xl text-secondary">Address</h3>
+                    <h3 className='text-secondary'>{user?.email}</h3>
+                    <h3 className="text-2xl text-secondary">Github Link</h3>
+                    <h3 className='text-secondary'>{user?.email}</h3>
+                    <button className='btn btn-primary mt-8'>Update Info</button>
+                </div>
+            </div>
+
         </div>
     );
 };
